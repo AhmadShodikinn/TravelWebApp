d421b23c0c981097fa32e140a7b227d9a70a475a
AhmadShodikinn <dikiahmad05@gmail.com>
2024-07-01 16:30:39 +0700

    add section destination, fixing layout size & start to create fleet section

--- Travel/resources/views/home.blade.php
+++ Travel/resources/views/home.blade.php
@@ -115,7 +115,7 @@
             <span class="absolute inset-0 text-transparent -z-10" style="text-shadow: 3px 0 0 #24565C, -3px 0 0 #24565C, 0 3px 0 #24565C, 0 -3px 0 #24565C, 2px 2px #24565C, -2px -2px 0 #24565C, 2px -2px 0 #24565C, -2px 2px 0 #24565C;">Menjelajahi destinasi, menciptakan kenangan</span>
             </h1>
             <div class="mt-[66px] pb-[30px] flex items-center justify-center gap-x-6">
-            <a href="#" class="relative rounded-[10px] h-[40px]   bg-[#24565C] px-5 py-2.5 text-sm font-medium text-white shadow-sm flex items-center justify-center overflow-hidden group">
+            <a href="#" class="relative rounded-[10px] h-[40px] bg-[#24565C] px-5 py-2.5 text-sm font-medium text-white shadow-sm flex items-center justify-center overflow-hidden group">
                 <span class="group-hover:bg-[#24565C]" aria-hidden="true" style="transition: background-color 0.3s ease;"></span>
                 <span class="relative z-10">Jelajahi Sekarang</span>
                 <img src="{{ asset('img/icon-arrow.svg') }}" alt="Arrow Icon" class="ml-4 h-4 w-4 opacity-2">
@@ -128,28 +128,28 @@
     <!-- End Image -->
     <!-- End Hero -->
     <!-- stats -->
-    <div class="bg-[#CCD6D4] py-16 sm:py-16 pt-60 px-6 lg:px-8">
-    <h2 class="text-center text-4xl font-bold tracking-tight text-[#24565C] sm:text-4xl">LAYANAN KAMI</h2>
-    <div class="mt-16 grid grid-cols-1 gap-x-7 gap-y-7 text-center lg:grid-cols-3  lg:mx-[100px]">
+    <div class="bg-[#CCD6D4] py-16 sm:py-12 pt-60 px-6 lg:px-8">
+    <h2 class="text-center text-3xl font-bold tracking-tight text-[#24565C] sm:text-3xl">LAYANAN KAMI</h2>
+    <div class="mt-8 grid grid-cols-1 gap-x-7 gap-y-7 text-center lg:grid-cols-3  lg:mx-[100px]">
         <div class="mx-auto max-w-xs lg:max-w-full lg:flex lg:items-center lg:gap-x-4">
-        <img src="{{ asset('img/icon-tour.svg') }}" alt="Vector Image" class="h-[100px] w-[100px] lg:w-auto">
+        <img src="{{ asset('img/icon-tour.svg') }}" alt="Vector Image" class="h-[80px] w-[80px] lg:w-auto">
             <div class="mt-4 lg:mt-0 ">
-                <h2 class="text-left text-2xl font-bold tracking-tight text-[#24565C]">Tur Pribadi</h2>
+                <h2 class="text-left text-xl font-bold tracking-tight text-[#24565C]">Tur Pribadi</h2>
                 <p class="text-left text-base text-gray-900">Melayani perjalanan khusus untuk membawa anda kemana saja.</p>
             </div>
         </div>
         <div class="mx-auto max-w-xs lg:max-w-full lg:flex lg:items-center lg:gap-x-4">
-        <img src="{{ asset('img/icon-briefcase.svg') }}" alt="Vector Image" class="h-[100px] w-[100px] lg:w-auto">
+        <img src="{{ asset('img/icon-briefcase.svg') }}" alt="Vector Image" class="h-[80px] w-[80px] lg:w-auto">
             <div class="mt-4 lg:mt-0 ">
-                <h2 class="text-left text-2xl font-bold tracking-tight text-[#24565C]">Tur Antar Kota</h2>
+                <h2 class="text-left text-xl font-bold tracking-tight text-[#24565C]">Tur Antar Kota</h2>
                 <p class="text-left text-base text-gray-900">Menyediakan tur Antar kota Malang-Juanda / Jawa-Bali.</p>
             </div>
-        </div>
+        </div>  
         <div class="mx-auto max-w-xs lg:max-w-full lg:flex lg:items-center lg:gap-x-4">
-        <img src="{{ asset('img/icon-car.svg') }}" alt="Vector Image" class="h-[100px] w-[100px] lg:w-auto">
+        <img src="{{ asset('img/icon-car.svg') }}" alt="Vector Image" class="h-[80px] w-[80px] lg:w-auto">
             <div class="mt-4 lg:mt-0 ">
-                <h2 class="text-left text-2xl font-bold tracking-tight text-[#24565C]">Sewa Kendaraan</h2>
-                <p class="text-left text-base text-gray-900">Menyediakan kendaraan beserta Supir untuk keperluan transportasi..</p>
+                <h2 class="text-left text-xl font-bold tracking-tight text-[#24565C]">Sewa Kendaraan</h2>
+                <p class="text-left text-base text-gray-900">Menyediakan kendaraan beserta Supir untuk keperluan transportasi.</p>
             </div>
         </div>
     </div>
@@ -158,44 +158,130 @@
 
     <!-- Destination -->
     <div class="py-16 sm:py-16 pt-60 px-6 lg:px-8">
-    <h2 class="text-center text-4xl font-bold tracking-tight text-[#24565C] sm:text-4xl">DESTINASI POPULER</h2>
+    <h2 class="text-center text-3xl font-bold tracking-tight text-[#24565C] sm:text-3xl">DESTINASI POPULER</h2>
     <p class="py-3 text-center text-lg font-medium text-gray-900">Berikut adalah beberapa destinasi populer yang sering dipesan di NUFI TRAVEL</p>
-    <div class="mt-16 grid grid-cols-1 gap-x-7 gap-y-7 text-center lg:grid-cols-4  lg:mx-[100px]">
-    
-    <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px]">
-        <img src="{{ asset('img/bg-bromo.jpg') }}" alt="Popular Destination" class="absolute inset-0 w-full h-full object-cover">
-        <div class="absolute inset-0 bg-black opacity-40"></div>
-        <div class="absolute inset-0 flex items-center justify-center">
-            <h3 class="text-3xl font-bold text-white">BROMO</h3>
+    <div class="mt-2 grid grid-cols-1 gap-x-6 gap-y-6 text-center lg:grid-cols-4 lg:mx-[100px]">
+        <!-- card -->
+        <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px] group">
+            <img src="{{ asset('img/bg-bromo.jpg') }}" alt="Popular Destination" class="absolute inset-0 w-full h-full object-cover">
+            <div class="absolute inset-0 bg-black opacity-25"></div>
+            <div class="absolute inset-0 flex flex-col items-center justify-center transition-transform duration-300 ease-in-out group-hover:translate-y-[-30px]">
+                <h3 class="text-2xl font-bold text-white mb-4">Bromo</h3>
+            </div>
+            <div class="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                <a href="#" class="relative rounded-lg h-[35px] w-[170px]  px-3 text-sm font-medium text-white shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#24565C] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Whatsapp</span>
+                </a>
+                <a href="#" class="relative rounded-lg h-[35px] w-[170px] px-3 text-sm font-medium text-[#24565C] shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#f7f7f7] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Wechat</span>
+                </a>
+            </div>
+        </div>
+        <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px] group">
+            <img src="{{ asset('img/bg-kawah-ijen.jpg') }}" alt="Popular Destination" class="absolute inset-0 w-full h-full object-cover">
+            <div class="absolute inset-0 bg-black opacity-25"></div>
+            <div class="absolute inset-0 flex flex-col items-center justify-center transition-transform duration-300 ease-in-out group-hover:translate-y-[-30px]">
+                <h3 class="text-2xl font-bold text-white mb-4">Kawah Ijen</h3>
+            </div>
+            <div class="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                <a href="#" class="relative rounded-lg h-[35px] w-[170px]  px-3 text-sm font-medium text-white shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#24565C] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Whatsapp</span>
+                </a>
+                <a href="#" class="relative rounded-lg h-[35px] w-[170px] px-3 text-sm font-medium text-[#24565C] shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#f7f7f7] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Wechat</span>
+                </a>
+            </div>
+        </div>
+        <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px] group">
+            <img src="{{ asset('img/bg-tumpak-sewu.jpg') }}" alt="Popular Destination" class="absolute inset-0 w-full h-full object-cover">
+            <div class="absolute inset-0 bg-black opacity-25"></div>
+            <div class="absolute inset-0 flex flex-col items-center justify-center transition-transform duration-300 ease-in-out group-hover:translate-y-[-30px]">
+                <h3 class="text-2xl font-bold text-white mb-4">Tumpak Sewu</h3>
+            </div>
+            <div class="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                <a href="#" class="relative rounded-lg h-[35px] w-[170px]  px-3 text-sm font-medium text-white shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#24565C] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Whatsapp</span>
+                </a>
+                <a href="#" class="relative rounded-lg h-[35px] w-[170px] px-3 text-sm font-medium text-[#24565C] shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#f7f7f7] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Wechat</span>
+                </a>
+            </div>
+        </div>
+        <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px] group">
+            <img src="{{ asset('img/bg-airport.jfif') }}" alt="Popular Destination" class="absolute inset-0 w-full h-full object-cover">
+            <div class="absolute inset-0 bg-black opacity-25"></div>
+            <div class="absolute inset-0 flex flex-col items-center justify-center transition-transform duration-300 ease-in-out group-hover:translate-y-[-30px]">
+                <h3 class="text-2xl font-bold text-white mb-4">Juanda</h3>
+            </div>
+            <div class="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                <a href="#" class="relative rounded-lg h-[35px] w-[170px]  px-3 text-sm font-medium text-white shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#24565C] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Whatsapp</span>
+                </a>
+                <a href="#" class="relative rounded-lg h-[35px] w-[170px] px-3 text-sm font-medium text-[#24565C] shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#f7f7f7] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Wechat</span>
+                </a>
+            </div>
         </div>
     </div>
-    <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px]">
-        <img src="{{ asset('img/bg-bromo.jpg') }}" alt="Popular Destination" class="absolute inset-0 w-full h-full object-cover">
-        <div class="absolute inset-0 bg-black opacity-40"></div>
-        <div class="absolute inset-0 flex items-center justify-center">
-            <h3 class="text-3xl font-bold text-white">BROMO</h3>
+    <p class="py-3 text-center text-lg font-medium text-gray-900">Kami juga melayani destinasi antar kota / antar provinsi, seperti</p>
+    <!-- column 2 -->
+    <div class="mt-2 grid grid-cols-1 gap-x-6 gap-y-6 text-center lg:grid-cols-2 lg:mx-[100px]">
+        <!-- card -->
+        <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px] group">
+            <img src="{{ asset('img/bg-malang.jfif') }}" alt="Popular Destination" class="absolute left-0 inset-0 w-1/2 h-full object-cover"><div class="absolute inset-0 bg-black opacity-10"></div>
+            <img src="{{ asset('img/bg-batu.jpg') }}" alt="Popular Destination" class="absolute left-2/4 inset-0 w-1/2 h-full object-cover"><div class="absolute inset-0 bg-black opacity-10"></div>
+            <div class="absolute inset-0 flex flex-col items-center justify-center transition-transform duration-300 ease-in-out group-hover:translate-y-[-30px]">
+                <h3 class="text-2xl font-bold text-white mb-4">Malang - Batu</h3>
+            </div>
+            <div class="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                <a href="#" class="relative rounded-lg h-[35px] w-[300px]  px-3 text-sm font-medium text-white shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#24565C] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Whatsapp</span>
+                </a>
+                <a href="#" class="relative rounded-lg h-[35px] w-[300px] px-3 text-sm font-medium text-[#24565C] shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#f7f7f7] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Wechat</span>
+                </a>
+            </div>
+        </div>
+        <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px] group">
+            <img src="{{ asset('img/bg-jawa.jpg') }}" alt="Popular Destination" class="absolute left-0 inset-0 w-1/2 h-full object-cover"><div class="absolute inset-0 bg-black opacity-10"></div>
+            <img src="{{ asset('img/bg-bali.jpg') }}" alt="Popular Destination" class="absolute left-2/4 inset-0 w-1/2 h-full object-cover"><div class="absolute inset-0 bg-black opacity-10"></div>
+            <div class="absolute inset-0 flex flex-col items-center justify-center transition-transform duration-300 ease-in-out group-hover:translate-y-[-30px]">
+                <h3 class="text-2xl font-bold text-white mb-4">Jawa - Bali</h3>
+            </div>
+            <div class="absolute bottom-6 left-1/2 transform -translate-x-1/2 flex flex-col items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+                <a href="#" class="relative rounded-lg h-[35px] w-[300px]  px-3 text-sm font-medium text-white shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#24565C] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Whatsapp</span>
+                </a>
+                <a href="#" class="relative rounded-lg h-[35px] w-[300px] px-3 text-sm font-medium text-[#24565C] shadow-sm flex items-center justify-center overflow-hidden">
+                    <span class="absolute inset-0 group-hover:bg-[#f7f7f7] transition-colors duration-300 ease-in-out"></span>
+                    <span class="relative z-10">Wechat</span>
+                </a>
+            </div>
         </div>
     </div>
-    <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px]">
-        <img src="{{ asset('img/bg-bromo.jpg') }}" alt="Popular Destination" class="absolute inset-0 w-full h-full object-cover">
-        <div class="absolute inset-0 bg-black opacity-40"></div>
-        <div class="absolute inset-0 flex items-center justify-center">
-            <h3 class="text-3xl font-bold text-white">BROMO</h3>
-        </div>
-    </div>
-    <div class="relative max-w-xs lg:max-w-full bg-white rounded-lg shadow-lg overflow-hidden lg:flex lg:items-center lg:gap-x-4 h-[400px]">
-        <img src="{{ asset('img/bg-bromo.jpg') }}" alt="Popular Destination" class="absolute inset-0 w-full h-full object-cover">
-        <div class="absolute inset-0 bg-black opacity-40"></div>
-        <div class="absolute inset-0 flex items-center justify-center">
-            <h3 class="text-3xl font-bold text-white">BROMO</h3>
-        </div>
-    </div>
-    </div>
+    <!-- end column 2 -->
     </div>
     <!-- end Destination -->
 
-</div>
+    <!-- Fleet -->
+    <div class="py-16 sm:py-16 pt-60 px-6 lg:px-8 bg-cover bg-center" style="background-image: url('/img/bg-vector.svg');">
+        <h2 class="text-center text-3xl font-bold tracking-tight text-[#24565C] sm:text-3xl">ARMADA KAMI</h2>
+        
     </div>
+       
+    </div>
+    <!-- end header -->
 </div>
 </body>
 </html>
